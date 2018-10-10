37549fdfd83f5f6dc49715a7b045332523a0615f
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-10 08:58:04 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter = 0
+    split_sentence = self.split(" ")
+    split_sentence.each do |word|
+      if word.question?||word.sentence?||word.exclamation?
+        counter += 1
+      end
+    end
+    counter
   end
 end
