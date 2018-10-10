c62fe064428d90f53ffe038f63c5d314f473b33a
yoshinator <yoanante@gmail.com>
2018-10-09 23:34:41 -0400

    Regex baby

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # "This, well, is a sentenc\e.\ This is to\o!\! And so is this, I thin\k?\ Wo\o...\"
+      self.split(/\S[.!?]/)
   end
 end
+
+binding.pry
+# string = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
