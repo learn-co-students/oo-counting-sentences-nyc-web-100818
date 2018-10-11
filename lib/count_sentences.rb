24ddbd0570394e637034662a3c188c0644c0e331
github username <github email address>
2018-10-11 14:25:08 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
+# require 'pry'
 
 class String
 
+#String #sentence? returns true if the string that you are calling this method on ends in a period.
+#
   def sentence?
-    
+    # if self.sentence[-1] == "."
+    #   true
+    # else
+    # end
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
+
+# binding.pry
